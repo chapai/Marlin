215bff1d9c4ebec92f6f554e53c19300fd618977
xxorza <xxorza@gmail.com>
2021-12-25 22:15:04 -0800

    tweaked

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -931,7 +931,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT { 200, 200, 800, 1600 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT { 200, 200, 800, 400 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1005,7 +1005,7 @@
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM         0.05 // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM         0.002 // (mm) Distance from real junction edge
   #define JD_HANDLE_SMALL_SEGMENTS            // Use curvature estimation instead of just the junction angle
                                               // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
